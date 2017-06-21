f933e3e531e68a4907d1aaef244a41217826a35d
Radium Zheng <ztl8702@msn.com>
2017-06-22 01:43:33 +1000

    Change folder structure to match that of current version

---
+++ workvm/Application1/Application1/Controllers/ParentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Application1;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Application2.Controllers
+{
+    public class ParentController : Controller
+    {
+        private Configuration ConfigSettings { get; set; }
+        // GET: /<controller>/
+        public ParentController(IOptions<Configuration> settings)
+        {
+            ConfigSettings = settings.Value;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                var order = ConfigSettings.record[i].Split(' ');
+                Business business = new Business(order);
+                new Thread(business.Fun).Start();
+            }
+            
+                
+            return View();
+        }
+    }
+}
