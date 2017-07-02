2b7b0cfd6f26fc9bc799fc304928f3e5b1af0a4c
Radium <ztl8702@users.noreply.github.com>
2017-07-02 20:20:40 +0800

    Update run.sh

--- workvm/Application3/run.sh
+++ workvm/Application3/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+docker-compose -f docker-compose.yml -f docker-compose.override.yml stop
 docker-compose -f docker-compose.yml -f docker-compose.override.yml build
 docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
 echo "**Run `docker-compose -f docker-compose.yml -f docker-compose.override.yml logs` to see output**"
