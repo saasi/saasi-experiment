eae5f94de7992f4062e5bc304e65942d1d9239c4
donydex <2508090629@qq.com>
2017-12-01 22:14:40 +0800

    Update run.sh
    sustc

--- workvm/Application3/run.sh
+++ workvm/Application3/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker-compose -f docker-compose.yml -f docker-compose.override.yml down
-docker-compose -f docker-compose.yml -f docker-compose.override.yml build
-docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.override.yml down //有多个-f参数可做替换操作，down:停止
+docker-compose -f docker-compose.yml -f docker-compose.override.yml build//重建镜像
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d//后台运行
 #echo "**Run `docker-compose -f docker-compose.yml -f docker-compose.override.yml logs` to see output**"
