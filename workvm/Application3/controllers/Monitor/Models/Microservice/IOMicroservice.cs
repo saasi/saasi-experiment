62ca6618f9fece5ad453e10d4126388b2a84f165
Radium Zheng <ztl8702@msn.com>
2017-12-09 02:17:02 +0800

    Cherry pick from master

---
+++ workvm/Application3/controllers/Monitor/Models/Microservice/IOMicroservice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Docker.DotNet;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Monitor
+{
+    public class IOMicroservice : Microservice
+    {
+        private static double IOViolationThresdhold = 30.0;
+        private int IOViolationCounter = 0;
+        public IOMicroservice(DockerClient dockerClient) : base(ContainerType.IOMicroservice, dockerClient)
+        {
+             
+        }
+
+        public override async Task DoScale()
+        {
+            await base.DoScale();
+            Console.WriteLine("scaleout io");
+            await RunScriptOnHost("scaleio1.sh", this.ScaleTarget);
+        }
+
+        public override void CheckResourceUtilisation()
+        {
+            foreach (var pair in Containers)
+            {
+                var container = pair.Value;
+                if (container.IOUsage > IOViolationThresdhold && container.IOUsage < 50)
+                {
+
+                    IOViolationCounter++;
+                    Console.WriteLine($"IO violation: {container.Id} Total {IOViolationCounter}");
+                }
+            }
+
+            if (IOViolationCounter >= 5 * ActualScale)
+            {
+                if (LastScaleTime.AddSeconds(30).CompareTo(DateTime.Now) < 0) //A container can scale one time in one minute.
+                {
+                    LastScaleTime = DateTime.Now;
+                    ScaleTarget++;
+                    Console.WriteLine($"IO -> {ScaleTarget}");
+                    WriteScaleOutRecord();
+                    DoScale();
+                }
+                IOViolationCounter = 0;
+            }
+        }
+
+
+    }
+}
---
+++ workvm/Application3/controllers/Monitor/Monitor.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using Docker.DotNet;
+using System.Threading.Tasks;
+using Docker.DotNet.Models;
+
+namespace Monitor
+{
+    class Monitor
+    {
+        private static DockerClient dockerClient;
+        private static string vmaddress;
+        private static IOMicroservice ioMicroservice;
+        private static MemoryMicroservice memoryMicroservice;
+        private static CPUMicroservice cpuMicroservice;
+        private static Dictionary<string, int> bms = new Dictionary<string, int>();
+        private static Dictionary<string, DateTime> scaleTime = new Dictionary<string, DateTime>();
+        private static int bmsNum = 1;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                dockerClient = new DockerClientConfiguration(new Uri("http://127.0.0.1:4243"))
+                                .CreateClient();
+            }
+            catch
+            {
+                Console.WriteLine("Could not connect to Docker Remote API.");
+                return;
+            }
+
+
+
+            ioMicroservice = new IOMicroservice(dockerClient); // monitor io_microservice
+            cpuMicroservice = new CPUMicroservice(dockerClient);// monitor cpu_microservice
+            memoryMicroservice = new MemoryMicroservice(dockerClient);// monitor memory_microservice
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine($"##########{DateTime.Now}##########");
+                Console.WriteLine($"CPU {cpuMicroservice.ActualScale}->{cpuMicroservice.ScaleTarget}");
+                Console.WriteLine($"IO {ioMicroservice.ActualScale}->{ioMicroservice.ScaleTarget}");
+                Console.WriteLine($"Memory {memoryMicroservice.ActualScale}->{memoryMicroservice.ScaleTarget}");
+                Console.WriteLine("###########################################");
+            }
+
+        }
+
+        public Monitor()
+        {
+          
+        }
+    }
+}
