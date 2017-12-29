1de76261050a25765df344afd93e8ecbba726734
chenshengduo <1134600061@qq.com>
2017-12-29 16:09:22 +0800

    generate IO/Memory/CPU stress
    create class for generate IO/Memory/CPU stress and the .net application that use these class

---
+++ src/Saasi.Shared/Saasi.Shared.Workload/Saasi.Shared.Workload.Cpu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Saasi.Shared.Workload
+{
+    public class GenerateCpuWorkload
+    {
+        public string Run(int time)
+        {
+            CpuProcess(time);
+            return $"OK. CPU task finished. Seconds run = {time}.";
+        }
+
+        private void CpuProcess(int time)
+        {
+            // simulate cpu bound operation for `time` seconds
+            DateTime currentTime = new DateTime();
+            currentTime = System.DateTime.Now;
+            DateTime finishTime = currentTime.AddSeconds(time);
+            Guid id = Guid.NewGuid();
+            Console.WriteLine(id + ":Start." + Convert.ToString(currentTime));
+            int i = 0;
+            while (System.DateTime.Now.CompareTo(finishTime) < 0)
+            {
+                string comparestring1 = StringDistance.GenerateRandomString(1000);
+                i++;
+                if (i == 50)
+                {
+                    Thread.Sleep(30); // Change the wait time here to adjust cpu usage.
+                    i = 0;
+                }
+            }
+            Console.WriteLine(id + ":Done." + Convert.ToString(System.DateTime.Now));
+        }
+
+        class StringDistance
+        {
+            public static string GenerateRandomString(int length)
+            {
+                var r = new Random((int)DateTime.Now.Ticks);
+                var sb = new StringBuilder(length);
+                for (int i = 0; i < length; i++)
+                {
+                    int c = r.Next(97, 123);
+                    sb.Append(Char.ConvertFromUtf32(c));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ src/Saasi.Shared/Saasi.Shared.Workload/Saasi.Shared.Workload.Memory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Saasi.Shared.Workload
+{
+    public class GenerateMemoryWorkload
+    {
+        public string Run(int time)
+        {
+            MemoryProcess(time);
+            return "OK";
+        }
+
+        public void MemoryProcess(int time)
+        {
+            //simulate memory use
+            DateTime currentTime = new DateTime();
+            currentTime = System.DateTime.Now;
+            DateTime finishTime = currentTime.AddSeconds(time);
+            Guid id = Guid.NewGuid();
+            Console.WriteLine(id + ":Start." + Convert.ToString(currentTime));
+            //List<IntPtr> alist = new List<IntPtr>();
+            List<byte[]> alist = new List<byte[]>();
+            int i = 0;
+            while (System.DateTime.Now.CompareTo(finishTime) < 0)
+            {
+                byte[] b = new byte[1024];
+                alist.Add(b); // Change the size here.
+
+                i++;
+                if (i == 2000)
+                {
+                    Thread.Sleep(200); // Change the wait time here to control memory usage.
+                    i = 0;
+
+                }
+            }
+            Console.WriteLine(id + ":Done." + Convert.ToString(System.DateTime.Now));
+            alist.Clear();
+            alist = null;
+            GC.Collect(); // release memory
+        }
+    }
+}
