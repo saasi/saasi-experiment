4cdd373d3d7bbc3c3512df69d7b43d622e151f68
Radium Zheng <ztl8702@msn.com>
2017-12-31 16:42:27 +0800

    Improve BusinessWeb

--- src/Saasi.Monolithic.BusinessWeb/Program.cs
+++ src/Saasi.Monolithic.BusinessWeb/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace BusinessSimulation
+namespace Saasi.Monolithic.BusinessWeb
 {
     public class Program
     {
--- src/Saasi.Monolithic.BusinessWeb/Startup.cs
+++ src/Saasi.Monolithic.BusinessWeb/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace BusinessSimulation
+namespace Saasi.Monolithic.BusinessWeb
 {
     public class Startup
     {
@@ -24,6 +24,7 @@ namespace BusinessSimulation
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
