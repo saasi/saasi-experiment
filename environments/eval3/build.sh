7134492af61b47ce9cc9479f7a2a6d8dfbdc0bd5
Radium Zheng <ztl8702@msn.com>
2018-01-02 17:36:28 +0800

    update eval3 configuration

--- environments/eval3/build.sh
+++ environments/eval3/build.sh
@@ -3,8 +3,8 @@ docker build -t regserv:5000/prometheus ./prometheus
 docker build -t regserv:5000/grafana ./grafana
 cd ../../src
 docker build -t regserv:5000/business_microservice ./BusinessMicroservice
-docker build -t regserv:5000/io_microservice ./Saasi.Microservices/Saasi.Microservices.Io
-docker build -t regserv:5000/cpu_microservice ./Saasi.Microservices/Saasi.Microservices.Cpu
-docker build -t regserv:5000/memory_microservice ./Saasi.Microservices/Saasi.Microservices.Memory
+docker build -t regserv:5000/io_microservice -f ./Saasi.Microservices/Saasi.Microservices.Io/Dockerfile ./
+docker build -t regserv:5000/cpu_microservice -f ./Saasi.Microservices/Saasi.Microservices.Cpu/Dockerfile ./
+docker build -t regserv:5000/memory_microservice -f ./Saasi.Microservices/Saasi.Microservices.Memory/Dockerfile ./
 docker build -t regserv:5000/docker-swarm-exporter ./docker-swarm-exporter
 docker build -t regserv:5000/autoscaler ./autoscaler
