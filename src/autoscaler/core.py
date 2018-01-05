bd3e40f1cd780e5b7b72dd8917964ca61460bc9f
Radium Zheng <ztl8702@msn.com>
2018-01-05 12:43:48 +0800

    ease out business scaling

--- src/autoscaler/core.py
+++ src/autoscaler/core.py
@@ -121,21 +121,36 @@ class MemoryMicroserviceMG(MicroserviceMonitoringGroup):
 class BusinessMicroserviceMG(MicroserviceMonitoringGroup):
     BUSINESS_VIOLATION_RATE_THRESHOLD = 0.20
 
-    def __init__(self):
+    def __init__(self, min_scale = 1, max_scale = 20):
         super().__init__('business_microservice')        
-        
+        self._min_scale = min_scale
+        self._max_scale = max_scale
+        self._scale_up_rule = utils.DelayedActionHelper()
+        self._scale_down_rule = utils.DelayedActionHelper()
+
     def _check(self):
         currentRate = self._res.GetBusinessViolationRate()
         currentScale = self._swarm.GetScaleTarget()
-        targetScale = currentScale
-        if (currentRate > 0.20):
-            targetScale = currentScale + 1
-        elif (currentRate < 0.05):
-            targetScale = currentScale - 1
-        if targetScale < 5:
-            targetScale = 5 # Lower bound
-        if (targetScale != currentScale):
-            self._do_scale(targetScale)
+        
+        if (currentRate > 0.30):
+            self._scale_up_rule.setActive()
+            if (self._scale_up_rule.activeFor().total_seconds() > 10):
+                targetScale = currentScale + 2
+                targetScale = limit_range(targetScale, self._min_scale, self._max_scale)
+                self._do_scale(targetScale)
+                self._scale_up_rule.setInactive()
+        else:
+            self._scale_up_rule.setInactive()
+
+        if (currentRate < 0.05):
+            self._scale_down_rule.setActive()
+            if (self._scale_down_rule.activeFor().total_seconds() > 30):
+                targetScale = currentScale - 1
+                targetScale = limit_range(targetScale, self._min_scale, self._max_scale)
+                self._do_scale(targetScale)
+                self._scale_down_rule.setInactive()
+        else:
+            self._scale_down_rule.setInactive()
 
 
 class DummyMG(MicroserviceMonitoringGroup):
