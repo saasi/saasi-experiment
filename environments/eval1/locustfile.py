358886d01765f3a98a86748d96583b0ac11105a5
Radium Zheng <ztl8702@msn.com>
2018-01-21 17:44:50 +0800

    [eval1] config

--- environments/eval1/locustfile.py
+++ environments/eval1/locustfile.py
@@ -5,60 +5,16 @@ from datetime import datetime
 random.seed(datetime.now())
 
 class BusinessUserBehaviour(TaskSet):
-
-    config = [
-        # io cpu memory timetorun timeout
-        [0, 1, 0, 1, 2],
-        [1, 0, 0, 1, 2],
-        [0, 0, 1, 1, 2],
-        [1, 1, 0, 1, 2],
-        [1, 0, 1, 1, 2],
-        [0, 1, 1, 5, 10],
-        [0, 1, 0, 5, 10],
-        [1, 0, 0, 5, 10],
-        [0, 0, 1, 5, 10],
-        [1, 1, 0, 5, 10],
-        [1, 0, 1, 5, 10],
-        [0, 1, 1, 5, 10],
-        [0, 1, 0, 10, 30],
-        [0, 0, 1, 10, 30],
-        [1, 1, 0, 10, 30],
-        [1, 0, 1, 10, 30],
-        [0, 1, 1, 10, 30],
-        [0, 1, 0, 15, 60],
-        [1, 0, 0, 15, 60],
-        [0, 0, 1, 15, 60],
-        [1, 1, 0, 15, 60],
-        [1, 0, 1, 15, 60],
-        [0, 1, 1, 15, 60],
-        [0, 1, 0, 20, 80],
-        [1, 0, 0, 20, 80],
-        [0, 0, 1, 20, 80],
-        [1, 1, 0, 20, 80],
-        [1, 0, 1, 20, 80],
-        [0, 1, 1, 20, 80]
-    ]
             
     def on_start(self):
         self.current_config_no = 0 # We will iterate through all the configs 
-        self._shuffled = BusinessUserBehaviour.config
-        random.shuffle(self._shuffled)
-
+        
     @task
-    def business_request(self):
-        current_config = self._shuffled[self.current_config_no]
-        run_io = current_config[0]
-        run_cpu = current_config[1]
-        run_memory = current_config[2]
-        time_to_run = current_config[3]
-        timeout = current_config[4]
-
-        current_timestamp =  int(time.time())
+    def business_request(self): 
+        self.client.get("/Business?operationId="+str(self.current_config_no))
         self.current_config_no = self.current_config_no + 1
-        if (self.current_config_no >= len(BusinessUserBehaviour.config)): # Finished a cycle, restart from the first config
+        if (self.current_config_no > 5):
             self.current_config_no = 0
-        self.client.get("/Business?io="+str(run_io)+"&cpu="+str(run_cpu)+"&memory="+str(run_memory)+"&timestart="+ str(current_timestamp) +"&timetorun="+str(time_to_run)+"&timeout="+str(timeout))
-
 
 class BusinessUser(HttpLocust):
     task_set = BusinessUserBehaviour
