5dd746048e9e727dedeac86b4f886414851d5716
Radium Zheng <ztl8702@msn.com>
2018-01-24 16:53:20 +0800

    quickfix

--- environments/eval2/export_data.sh
+++ environments/eval2/export_data.sh
@@ -5,9 +5,7 @@ TIME_SPAN=$1
 OUTPUT_DIR=$2
 
 $STYX -d $TIME_SPAN 'bms_business_violation_total' > $OUTPUT_DIR/bms_business_violation_total.csv
-$STYX -d $TIME_SPAN 'docker_swarm_service_target_replicas{service_name=~"\\w+_memory_microservice"}' > $OUTPUT_DIR/scale_memory.csv
-$STYX -d $TIME_SPAN 'docker_swarm_service_target_replicas{service_name=~"\\w+_cpu_microservice"}' > $OUTPUT_DIR/scale_cpu.csv
-$STYX -d $TIME_SPAN 'docker_swarm_service_target_replicas{service_name=~"\\w+_io_microservice"}' > $OUTPUT_DIR/scale_io.csv
+$STYX -d $TIME_SPAN 'docker_swarm_service_target_replicas{service_name=~"\\w+_combined_microservice"}' > $OUTPUT_DIR/scale_memory.csv
 $STYX -d $TIME_SPAN 'docker_swarm_service_target_replicas{service_name=~"\\w+_business_microservice"}' > $OUTPUT_DIR/scale_business.csv
 $STYX -d $TIME_SPAN 'avg(rate(container_cpu_usage_seconds_total[1m])) by (container_label_com_docker_swarm_service_name) * 100' > $OUTPUT_DIR/cpu_usage.csv
 $STYX -d $TIME_SPAN 'avg(container_memory_rss) by (container_label_com_docker_swarm_service_name)' > $OUTPUT_DIR/memory_usage.csv
